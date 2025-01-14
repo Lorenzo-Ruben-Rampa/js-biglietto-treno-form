f24165e1df6cf244ff2b0a69b89a6d5103d6a6bc
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-14 08:27:11 +0100

    ricreo repo - riparto da zero

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let chilometri = prompt("Inserisci il numero di km da percorrere: ");
+let age = prompt("Inserisci la tua età: ");
+
+console.log();
+
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
