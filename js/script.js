c82080cd9aa38401b1459f887befe200b5bc818a
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-15 19:03:52 +0100

    tentativo autonomo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-let chilometri = prompt("Inserisci il numero di km da percorrere: ");
-let age = prompt("Inserisci la tua età: ");
+const name = document.getElementById("name").value;
+const chilometri = parseInt(document.getElementById("km").value);
+const age = parseInt(document.getElementById("age").value);
+const prezzoBase = chilometri *0.21;
+let prezzoFinale = prezzoBase;
 
-console.log();
+// Condizioni sconto
+if (age<18) {
+   prezzoFinale * 0.80;
+} else if(age > 65) {
+   prezzoFinale * 0.60;
+} 
+
+console.log(prezzoFinale);
+
+//Formatto prezzo finale a decimali
+prezzoFinale = prezzoFinale.toFixed(2);
+
+//Inserisco i risultati nel secondo container
+const nomePasseggero = document.querySelector('outputName')
+nomePasseggero.innerHTML += '<p></p>';
+nomePasseggero.append('name');
+
+const datoPrezzoFinale = document.querySelector('outputName')
+datoPrezzoFinale.innerHTML += '<p></p>'
+datoPrezzoFinale.append('prezzoFinale');
+
 
 // - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
+
